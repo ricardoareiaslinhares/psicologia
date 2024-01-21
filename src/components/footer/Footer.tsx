0430633007435ee6b45721884b78b3022e3688e4
Ricardo Linhares <ricardo@Air-de-Ana.home>
2024-01-21 23:35:01 +0000

    better h maps/video/info

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -15,15 +15,13 @@ const Footer = ({}: Props) => {
   const [buttonShow, setButtonShow] = useState<number>(1);
   const [containerHeight, setContainerHeight] = useState(490);
 
-
-
   useEffect(() => {
     if (buttonShow === 1) {
       setContainerHeight(400);
     } else if (buttonShow === 2) {
-      setContainerHeight(300);
+      setContainerHeight(270);
     } else {
-      setContainerHeight(490);
+      setContainerHeight(500);
     }
   }, [buttonShow]);
 
@@ -32,12 +30,14 @@ const Footer = ({}: Props) => {
       <h2 className="text-center sm:text-left text-xl font-bold">Contactos</h2>
 
       <div className="flex flex-1 flex-col lg:flex-row justify-between gap-8 flex-wrap mt-4 ">
-        <div className="flex flex-1  justify-center lg:justify-start w-auto flex-col px-4 sm:px-0" >
-  <TogetherAction/>
+        <div className="flex flex-1  justify-center lg:justify-start w-auto flex-col px-4 sm:px-0">
+          <TogetherAction />
           <div className="my-6 w-4/5 md:w-full flex self-center">
             <Separator className="bg-foreground/10" />
           </div>
-          <h4 className="text-md font-bold mb-3 px-2 sm:px-0 italic">Deixar mensagem...</h4>
+          <h4 className="text-md font-bold mb-3 px-2 sm:px-0 italic">
+            Deixar mensagem...
+          </h4>
           <Form />
         </div>
         <div className="mt-6 flex md:hidden w-4/5 self-center">
@@ -46,15 +46,24 @@ const Footer = ({}: Props) => {
         <div className="flex items-center self-center justify-center flex-1 flex-row flex-wrap  xl:flex-nowrap  gap-4 w-full ">
           <div className="sm:pl-2 items-center flex md:basis-3/4">
             {/* <img alt="imagem card" src="https://picsum.photos/500/300"></img> */}
-            <div className="flex items-center" style={{ height: `${containerHeight}px`, transition: 'height 0.3s ease-in-out' }}>
-      {buttonShow === 1 ? (
-        <MapGoogle />
-      ) : buttonShow === 2 ? (
-        <Player playing={true} url={'https://www.youtube.com/watch?v=14QkzMWrEqE&t=4s'} />
-      ) : (
-        <Info />
-      )}
-    </div>
+            <div
+              className="flex items-center"
+              style={{
+                height: `${containerHeight}px`,
+                transition: "height 0.3s ease-in-out",
+              }}
+            >
+              {buttonShow === 1 ? (
+                <MapGoogle />
+              ) : buttonShow === 2 ? (
+                <Player
+                  playing={true}
+                  url={"https://www.youtube.com/watch?v=14QkzMWrEqE&t=4s"}
+                />
+              ) : (
+                <Info />
+              )}
+            </div>
           </div>
 
           <div className="flex flex-1 md:basis-1/4 justify-center">
@@ -63,7 +72,7 @@ const Footer = ({}: Props) => {
                 className={`w-36 ${
                   buttonShow === 1 ? "bg-primary/70" : "bg-primary"
                 }`}
-                onClick={() => setButtonShow((prev) => prev = 1)}
+                onClick={() => setButtonShow((prev) => (prev = 1))}
               >
                 Localização
               </Button>
@@ -71,7 +80,7 @@ const Footer = ({}: Props) => {
                 className={`w-36 ${
                   buttonShow === 2 ? "bg-primary/70" : "bg-primary"
                 }`}
-                onClick={() => setButtonShow((prev) => prev = 2)}
+                onClick={() => setButtonShow((prev) => (prev = 2))}
               >
                 Ver Consultório
               </Button>
@@ -79,7 +88,7 @@ const Footer = ({}: Props) => {
                 className={`w-36 ${
                   buttonShow === 3 ? "bg-primary/70" : "bg-primary"
                 }`}
-                onClick={() => setButtonShow((prev) => prev = 3)}
+                onClick={() => setButtonShow((prev) => (prev = 3))}
               >
                 Horário e Preçário
               </Button>
