2c41207338254b52a8d019965771edbf44e12a62
Ricardo Linhares <ricardo@Air-de-Ana.home>
2024-01-22 17:27:11 +0000

    seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,11 +6,13 @@ import FullNavbar from "@/components/navbar/FullNavbar";
 import { ThemeProvider } from "@/components/ThemeProvider";
 import FooterCredits from "@/components/footer/FooterCredits";
 import { Separator } from "@/components/ui/separator";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://ricardolinhares.pt"),
+  
   title: "Ricardo Linhares Consultas de Psicologia Barcelos",
   description: "Psicologia Clínica / Psicoterapia em Barcelos. Não precisa de sofrer com depressão, ansiedade, pânico ou outros problemas. Há formas de lidar com eles, utilize a psicologia para viver com maiis vitalidade.",
 
@@ -23,6 +25,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+      <meta name="robots" content="all" />
+      </Head>
       <body className={inter.className}>
            <ThemeProvider
           attribute="class"
