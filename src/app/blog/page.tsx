1e62f4c8ebd9c66d0bf6a79da9137ea0a827569d
Ricardo Linhares <ricardo@Air-de-Ana.home>
2024-01-22 13:26:46 +0000

    design fixs

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -27,12 +27,12 @@ const Blogs = async ({}) => {
   //console.log(blogPosts[0])
   return (
     <main className="flex min-h-screen flex-col items-center justify-center  w-full z-0 overflow-hidden py-12 bg-muted  ">
-      <div className='flex flex-col gap-y-4 items-center mb-12'>
+      <div className='flex flex-col gap-y-4 items-center justify-center mb-12'>
       <h1 className='text-2xl font-bold text-foreground'>Livro Aberto de Psicologia</h1>
       <h3 className='text-lg font-bold text-primary px-4'>Artigos sobre psicologia, psicoterapia e saúde mental</h3>
 
       </div>
-      <section className="flex  w-full max-w-screen-2xl  px-8 md:px-24  flex-wrap flex-1 gap-6 justify-center md:justify-start items-center  ">
+      <section className="flex flex-1 w-full max-w-screen-2xl  px-8 md:px-24  flex-wrap gap-6 justify-center items-center ">
 
           {blogPosts.map((post:any, index:number) => {
             const poster = post.fields as BlogPost;
