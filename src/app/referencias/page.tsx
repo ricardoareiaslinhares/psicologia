0cc34491b3e974a665c7de712adb46e65d6cd196
Ricardo Linhares <ricardo@Air-de-Ana.home>
2024-01-23 00:47:22 +0000

    refs

--- src/app/referencias/page.tsx
+++ src/app/referencias/page.tsx
@@ -5,6 +5,8 @@ type Props = {}
 
 export default function Referencias({}: Props) {
   const references = refs.refs as { [key: string]: string };
+  const referencesPersonalidade = refs.refs2 as { [key: string]: string };
+
 
   const RenderReferences = () => {
     const renderedReferences = [];
@@ -23,13 +25,34 @@ export default function Referencias({}: Props) {
     );
   };
 
+  const RenderReferencesPersonalidade = () => {
+    const renderedReferences = [];
+
+    for (let i = 0; i < 4; i++) {
+      const iString = i.toString();
+      renderedReferences.push(referencesPersonalidade[iString]);
+    }
+    console.log(renderedReferences)
+    return (
+      <div className="flex flex-col gap-y-3 px-4 ">
+        {renderedReferences.map((item, index) => {
+     
+          return <p className="text-md mt-2 text-secondary -indent-9 whitespace-normal break-words" key={index}>{item}</p>;
+        })}
+      </div>
+    );
+  };
 
 
   return (
-    <main className="flex  flex-col items-center justify-between   w-screen max-w-screen-2xl z-0 overflow-hidden  ">
+    <main className="flex  flex-col items-center justify-between   w-screen max-w-screen-2xl z-0 overflow-hidden pb-8 ">
       <section className="flex  w-full max-w-screen-xl px-8 md:px-24 pt-8 flex-col gap-6">
       <h1 className={`text-lg font-bold text-center sm:text-left `}>{refs.titulo}</h1> 
         <RenderReferences />
+      </section>
+      <section className="flex  w-full max-w-screen-xl px-8 md:px-24 pt-8 flex-col gap-6">
+      <h1 className={`text-lg font-bold text-center sm:text-left `}>{refs.tituloBlogPersonalidade}</h1> 
+        <RenderReferencesPersonalidade />
       </section>
     </main>
   );
